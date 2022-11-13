5a1d8448c58709974f349ea32674f7c8dfbfeeb7
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-13 23:15:06 +0100

    feat: add service extensions

---
+++ Timespace.Api/ConfigureServices.cs
@@ -0,0 +1,26 @@
+﻿using NodaTime;
+using Timespace.Api.Infrastructure.Services;
+
+namespace Timespace.Api;
+
+public static class ConfigureServices
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IClock, DateTimeProvider>();
+    }
+
+    public static void AddAspnetServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+    
+    public static void AddConfiguration(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        // services.Configure<AuthenticationSettings>(configuration.GetSection(AuthenticationSettings.SectionName));
+        // services.Configure<SessionCookieSettings>(configuration.GetSection(SessionCookieSettings.SectionName));
+    }
+}
