40e7cd3259f575c7529600d52caad9a36f854a64
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-20 13:49:02 +0100

    feat: Add base exception

---
+++ Timespace.Api/Infrastructure/Errors/IBaseException.cs
@@ -0,0 +1,24 @@
+﻿namespace Timespace.Api.Infrastructure.Errors;
+
+public interface IBaseException
+{
+    public string Type { get; }
+    public int StatusCode { get; }
+    public string Title { get; }
+    public string? Detail { get; }
+    public Dictionary<string, object?> Extensions { get; }
+}
+
+public class TestException : Exception, IBaseException
+{
+    public TestException(string extension)
+    {
+        Extensions["test"] = extension;
+    }
+    
+    public string Type { get; } = "test-type";
+    public int StatusCode { get; } = StatusCodes.Status403Forbidden;
+    public string Title { get; } = "Test title";
+    public string? Detail { get; } = null;
+    public Dictionary<string, object?> Extensions { get; } = new();
+}
