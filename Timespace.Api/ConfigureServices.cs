ce9b618d401ddf608fd50a7d74615989a0f8cd4f
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-20 13:50:15 +0100

    feat: add problemdetails

--- Timespace.Api/ConfigureServices.cs
+++ Timespace.Api/ConfigureServices.cs
@@ -1,5 +1,9 @@
-﻿using NodaTime;
+﻿using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+using Timespace.Api.Infrastructure.Errors;
 using Timespace.Api.Infrastructure.Services;
+using ProblemDetailsOptions = Hellang.Middleware.ProblemDetails.ProblemDetailsOptions;
 
 namespace Timespace.Api;
 
@@ -8,8 +12,59 @@ public static class ConfigureServices
     public static void AddServices(this IServiceCollection services)
     {
         services.AddSingleton<IClock, DateTimeProvider>();
+        services.AddProblemDetails(ConfigureProblemDetails);
+        //services.AddTransient<ProblemDetailsFactory, TimespaceProblemDetailsFactory>();
     }
 
+    private static void ConfigureProblemDetails(ProblemDetailsOptions options)
+    {
+        options.IncludeExceptionDetails = (ctx, ex) => false;
+        // Custom mapping function for FluentValidation's ValidationException.
+        // options.MapFluentValidationException();
+
+        // You can configure the middleware to re-throw certain types of exceptions, all exceptions or based on a predicate.
+        // This is useful if you have upstream middleware that needs to do additional handling of exceptions.
+        options.Rethrow<NotSupportedException>();
+
+        options.MapToStatusCode<NotImplementedException>(StatusCodes.Status501NotImplemented);
+
+        options.MapToStatusCode<HttpRequestException>(StatusCodes.Status503ServiceUnavailable);
+        
+        options.Map<Exception>((context, ex) =>
+        {
+            if(ex is IBaseException baseException)
+            {
+                var problemDetails = new ProblemDetails()
+                {
+                    Type = baseException.Type,
+                    Title = baseException.Title,
+                    Status = baseException.StatusCode,
+                    Instance = context.Request.Path,
+                    Detail = baseException.Detail,
+                };
+                
+                foreach(KeyValuePair<string, object?> kvpair in baseException.Extensions)
+                {
+                    problemDetails.Extensions.Add(kvpair);
+                }
+
+                return problemDetails;
+            }
+
+            var internalErrorProblemDetails = new ProblemDetails()
+            {
+                Type = "internal-server-error",
+                Title = "Internal server error",
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = "An internal server error occurred.",
+                Instance = context.Request.Path,
+            };
+
+            return internalErrorProblemDetails;
+        });
+    }
+
+    
     public static void AddAspnetServices(this IServiceCollection services)
     {
         services.AddControllers();
