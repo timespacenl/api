9241f9c43c8573921bbe66c896550d5430d36b6b
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-26 14:40:30 +0100

    get registration flow query

---
+++ Timespace.Api/Application/Features/Authentication/Registration/Queries/GetRegistrationFlow.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+using Timespace.Api.Application.Features.Authentication.Registration.Common.Exceptions;
+using Timespace.Api.Infrastructure.Persistence;
+
+namespace Timespace.Api.Application.Features.Authentication.Registration.Queries;
+
+public static class GetRegistrationFlow {
+    public record Query(
+        [property:FromRoute(Name = "flowId")] Guid FlowId
+    ) : IRequest<Response>;
+    
+    public record Response(
+        Guid FlowId,
+        string NextStep
+    );
+    
+    public class Handler : IRequestHandler<Query, Response>
+    {
+        private readonly AppDbContext _db;
+        private readonly IClock _clock;
+    
+        public Handler(AppDbContext db, IClock clock)
+        {
+            _db = db;
+            _clock = clock;
+        }
+    
+        public Task<Response> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var flow = _db.RegistrationFlows.FirstOrDefault(x => x.Id == request.FlowId);
+            if (flow == null)
+                throw new FlowNotFoundException();
+
+            if (flow.ExpiresAt < _clock.GetCurrentInstant())
+                throw new FlowExpiredException();
+            
+            return Task.FromResult(new Response(flow.Id, flow.NextStep));
+        }
+    }
+    
+    public class QueryValidator : AbstractValidator<Query>
+    {
+        public QueryValidator()
+        {
+            
+        }
+    }
+}
