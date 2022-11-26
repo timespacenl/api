34168f4cd235fe21349cc24136da861081d10bb9
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-26 14:36:50 +0100

    add validation

---
+++ Timespace.Api/Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Timespace.Api.Application.Common.Attributes;
+using ValidationException = Timespace.Api.Application.Common.Exceptions.ValidationException;
+
+namespace Timespace.Api.Application.Common.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        if (request.GetType().GetCustomAttribute<SkipValidationAttribute>() != null)
+        {
+            return await next();
+        }
+        
+        var context = new ValidationContext<TRequest>(request);
+        var errorsDictionary = _validators
+            .Select(x => x.Validate(context))
+            .SelectMany(x => x.Errors)
+            .Where(x => x != null)
+            .GroupBy(
+                x => x.PropertyName,
+                x => x.ErrorMessage,
+                (propertyName, errorMessages) => new
+                {
+                    Key = propertyName,
+                    Values = errorMessages.Distinct().ToArray()
+                })
+            .ToDictionary(x => x.Key, x => x.Values);
+        
+        if (errorsDictionary.Any())
+        {
+            throw new ValidationException(errorsDictionary);
+        }
+        return await next();
+    }
+}
---
+++ Timespace.Api/Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,17 @@
+﻿using Timespace.Api.Infrastructure.Errors;
+
+namespace Timespace.Api.Application.Common.Exceptions;
+
+public class ValidationException : Exception, IBaseException
+{
+    public ValidationException(IReadOnlyDictionary<string, string[]> errorsDictionary)
+    {
+        Extensions["errors"] = errorsDictionary;
+    }
+
+    public string Type => "validation-error";
+    public int StatusCode => StatusCodes.Status422UnprocessableEntity;
+    public string Title => "Validation error";
+    public string? Detail => null;
+    public Dictionary<string, object?> Extensions { get; } = new();
+}
