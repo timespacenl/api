22fd5b76659601628e048f6615723b8a7c054c73
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-26 14:40:47 +0100

    add way to register pipeline behaviours to the service collection

---
+++ Timespace.Api/Application/Common/Behaviours/BehaviourServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Timespace.Api.Application.Common.Behaviours;
+
+public static class BehaviourServiceCollectionExtensions
+{
+    public static void RegisterBehaviours(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+    }
+}
