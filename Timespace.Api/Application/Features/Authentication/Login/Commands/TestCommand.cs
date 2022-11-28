f9ef4a4004e6b8801126e8f53a7e83c326f7ec3d
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-28 17:17:26 +0100

    delete test classes

---
+++ Timespace.Api/Application/Features/Authentication/Login/Commands/TestCommand.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using Timespace.Api.Infrastructure.Persistence;
+
+namespace Timespace.Api.Application.Features.Authentication.Login.Commands;
+
+public static class TestOperation {
+    public record Command(string TestName) : IRequest<Response>;
+    
+    public record Response(string Test);
+    
+    public class Handler : IRequestHandler<Command, Response>
+    {
+        private readonly AppDbContext _db;
+    
+        public Handler(AppDbContext db)
+        {
+            _db = db;
+        }
+    
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            return new Response("Test");
+        }
+    }
+    
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.TestName).NotEmpty().MaximumLength(5).WithMessage("Maximum length is 5");
+        }
+    }
+}
