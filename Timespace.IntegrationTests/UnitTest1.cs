9c73dcaecb38e8cc5c1213cb604017d827602a18
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-28 17:24:45 +0100

    Add integration test project

---
+++ Timespace.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Timespace.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
