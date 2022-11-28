14fefe80392dca1726c8c84408e9905527563dbc
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-28 17:43:42 +0100

    add integration test base

---
+++ Timespace.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Timespace.Api.Infrastructure.Persistence;
+
+namespace Timespace.IntegrationTests;
+
+internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            var integrationConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            configurationBuilder.AddConfiguration(integrationConfig);
+        });
+        
+        builder.ConfigureServices((webHostBuilderContext, services) =>
+        {
+            services
+                .Remove<DbContextOptions<AppDbContext>>()
+                .AddDbContext<AppDbContext>((sp, options) =>
+                    options.UseNpgsql(webHostBuilderContext.Configuration.GetConnectionString("DefaultConnection"),
+                        npgsqlDbContextOptionsBuilder => npgsqlDbContextOptionsBuilder.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+        });
+    }
+}
---
+++ Timespace.IntegrationTests/Testing.cs
@@ -0,0 +1,82 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Respawn;
+using Respawn.Graph;
+using Timespace.Api.Infrastructure.Persistence;
+
+namespace Timespace.IntegrationTests;
+
+[SetUpFixture]
+public partial class Testing
+{
+    private static WebApplicationFactory<Program> _factory = null!;
+    private static IConfiguration _configuration = null!;
+    private static IServiceScopeFactory _scopeFactory = null!;
+    private static Respawner _respawner = null!;
+
+    [OneTimeSetUp]
+    public async Task RunBeforeAnyTests()
+    {
+        _factory = new CustomWebApplicationFactory();
+        _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
+        _configuration = _factory.Services.GetRequiredService<IConfiguration>();
+        
+        _respawner = await Respawner.CreateAsync(_configuration.GetConnectionString("DefaultConnection")!, new RespawnerOptions()
+        {
+            TablesToIgnore = new Table[] { "__EFMigrationsHistory" }
+        });
+    }
+
+    public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        return await mediator.Send(request);
+    }
+    
+    public static async Task ResetState()
+    {
+        await _respawner.ResetAsync(_configuration.GetConnectionString("DefaultConnection")!);
+    }
+    
+    public static async Task<TEntity?> FindAsync<TEntity>(params object[] keyValues)
+        where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        return await context.FindAsync<TEntity>(keyValues);
+    }
+
+    public static async Task AddAsync<TEntity>(TEntity entity)
+        where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        context.Add(entity);
+
+        await context.SaveChangesAsync();
+    }
+
+    public static async Task<int> CountAsync<TEntity>() where TEntity : class
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        return await context.Set<TEntity>().CountAsync();
+    }
+
+    [OneTimeTearDown]
+    public void RunAfterAnyTests()
+    {
+    }
+}
