141e4bcc02f89d25b7d2c73bd53d882126f557ba
Duke <40759437+dukesteen@users.noreply.github.com>
2022-11-28 17:19:47 +0100

    add registration flow migration

---
+++ Timespace.Api/Migrations/20221126141708_AddRegistrationFlow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace Timespace.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegistrationFlow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RegistrationFlows",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<Instant>(type: "timestamp with time zone", nullable: false),
+                    NextStep = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    CompanyIndustry = table.Column<string>(type: "text", nullable: true),
+                    CompanySize = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrationFlows", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegistrationFlows");
+        }
+    }
+}
