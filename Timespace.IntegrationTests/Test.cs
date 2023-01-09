de2a1e461decf7ec89ef989b15390fb7fdf8c2a3
dukesteen <contact@steenbakkers.cc>
2023-01-09 22:19:59 +0100

    a

---
+++ Timespace.IntegrationTests/Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Timespace.IntegrationTests;
+
+public class Test : IntegrationTest
+{
+    [Fact]
+    public async Task Test1()
+    {
+        GetService(out IConfiguration configuration);
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public Test(SharedFixture sharedFixture) : base(sharedFixture)
+    {
+    }
+}
