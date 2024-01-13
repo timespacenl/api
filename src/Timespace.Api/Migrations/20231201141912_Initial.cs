133f2226d7ce1ca2db149caa14d2995764349bec
Duke <commits@dmail.icu>
2024-01-13 15:56:30 +0100

    Remove mediatr source generators

--- src/Timespace.Api/Migrations/20231201141912_Initial.cs
+++ src/Timespace.Api/Migrations/20231201141912_Initial.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
--- src/Timespace.SourceGenerators/Attributes.cs
+++
@@ -1,6 +0,0 @@
-namespace Timespace.SourceGenerators;
-
-[System.AttributeUsage(System.AttributeTargets.Class)]
-public class GenerateMediatrAttribute : System.Attribute
-{
-}
